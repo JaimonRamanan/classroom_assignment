34c1e30ad0c570027db870fc0258bff70efe9f67
jaimon <dev.jaimon@gmail.com>
2022-04-13 23:34:30 +0530

    path changed

--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,4 @@
-import 'package:classroom_assignment/modules/class_rooms/data/models/route_names.dart';
+import 'package:classroom_assignment/modules/common/models/route_names.dart';
 import 'package:classroom_assignment/modules/common/presentation/pages/home_screen.dart';
 import 'package:flutter/material.dart';
 
