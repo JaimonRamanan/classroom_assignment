8bd4b7bf77954ed61e8ea7f5acc1f0ec46077d95
jaimon <dev.jaimon@gmail.com>
2022-04-13 23:38:30 +0530

    subjects data integration

--- lib/modules/subjects/presentation/pages/subjects.dart
+++ lib/modules/subjects/presentation/pages/subjects.dart
@@ -1,4 +1,5 @@
 import 'package:classroom_assignment/modules/common/presentation/widgets/name_card.dart';
+import 'package:classroom_assignment/modules/subjects/data/models/subjects_model.dart';
 import 'package:classroom_assignment/modules/subjects/presentation/cubit/subjects_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,16 +14,25 @@ class Subjects extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Students'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(20.0),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-        itemCount: 2,
-        itemBuilder: (BuildContext context, index) {
-          return NameCard(title: 'classRoomsList[index].name');
+      body: BlocBuilder<SubjectsCubit, SubjectsState>(
+        builder: (context, state) {
+          if (state is SubjectsLoading) {
+          } else if (state is SubjectsLoaded) {
+            List<SubjectsModel> subjects = state.subjects;
+            return GridView.builder(
+              padding: const EdgeInsets.all(20.0),
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200,
+                  childAspectRatio: 3 / 2,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20),
+              itemCount: 2,
+              itemBuilder: (BuildContext context, index) {
+                return NameCard(title: subjects[index].name);
+              },
+            );
+          }
+          return Container();
         },
       ),
     );
